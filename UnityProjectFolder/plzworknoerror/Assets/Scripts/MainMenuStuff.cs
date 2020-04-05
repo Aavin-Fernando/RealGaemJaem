e36ad708f24bf188374805a0f47fa0060982bd55
Julian <progamerguy005@gmail.com>
2020-04-05 17:51:36 +1000

    Finished the base build of the main menu - NEW UI STILL NEEDED
    All scripts done. New UI still needed

---
+++ UnityProjectFolder/plzworknoerror/Assets/Scripts/MainMenuStuff.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuStuff : MonoBehaviour
+{
+    public void Exit(){
+        Application.Quit();
+    }
+    
+    public void LoadLevel(int i){
+        SceneManager.LoadScene(i);
+    }
+}
